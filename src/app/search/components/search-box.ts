c085580db7de2bbc44842eb0cd9c30cd14b02995
tkarling <tkarling@live.com>
2016-04-02 11:40:47 -0700

    user can search items via search box on header

--- src/app/search/components/search-box.ts
+++ src/app/search/components/search-box.ts
@@ -1,18 +1,25 @@
 import {Component, Output, EventEmitter} from "angular2/core";
 
 @Component({
-    selector:'search-box',
-    template:`<div>
-        <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" id="search" #input type="text" (input)="update.emit(input.value)">
-            <label class="mdl-textfield__label" for="search">Search</label>
+    selector: 'search-box',
+    template: `
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                  mdl-textfield--floating-label mdl-textfield--align-right">
+        <label class="mdl-button mdl-js-button mdl-button--icon"
+               for="search">
+          <i class="material-icons">search</i>
+        </label>
+        <div class="mdl-textfield__expandable-holder">
+          <input class="mdl-textfield__input" #input type="text" (input)="update.emit(input.value)"
+                 id="search">
         </div>
     </div>`
 })
-export class SearchBox{
+export class SearchBox {
     @Output() update = new EventEmitter();
 
-    ngOnInit(){
+    ngOnInit() {
         this.update.emit('');
     }
 }
+
