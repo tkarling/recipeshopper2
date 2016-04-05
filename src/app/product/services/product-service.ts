2876694142e930b1fff556ddb7fd27418790fbb2
tkarling <tkarling@live.com>
2016-04-05 11:14:12 -0700

    improved cross-browser behaviour

--- src/app/product/services/product-service.ts
+++ src/app/product/services/product-service.ts
@@ -6,18 +6,24 @@ import {BoughtStatus,
 
 @Injectable()
 export class ProductService {
-    products:ProductModel[];
-    //= [
-    //    new ProductModel("bread", GRAINS),
-    //    new ProductModel("butter", DAIRY),
-    //    new ProductModel("tomatoes", VEGGIES_FRUIT)
-    //];
+    products:ProductModel[] = [];
     editingProduct:ProductModel = null;
 
     constructor() {
-        this.products = JSON.parse(localStorage.getItem("products"));
+        if (localStorage) {
+            const productsInLocalStorage = JSON.parse(localStorage.getItem("products"));
+            productsInLocalStorage.forEach((product) => {
+                this.products.push(this.deserialize(product, ProductModel));
+            });
+        } else {
+            console.log('no local storage');
+            this.products = [
+                new ProductModel("bread", GRAINS),
+                new ProductModel("butter", DAIRY),
+                new ProductModel("tomatoes", VEGGIES_FRUIT)
+            ];
+        }
     }
-
 
     editing(product:ProductModel) {
         return this.editingProduct === product;
@@ -59,4 +65,20 @@ export class ProductService {
 
         this.updateProduct(product, toggledProduct);
     }
+
+    deserialize(json, clazz) {
+        var instance = new clazz();
+        for (var prop in json) {
+            if (!json.hasOwnProperty(prop)) {
+                continue;
+            }
+            if (typeof json[prop] === 'object') {
+                instance[prop] = this.deserialize(json[prop], clazz[prop]);
+            } else {
+                instance[prop] = json[prop];
+            }
+        }
+        return instance;
+    }
+
 }
