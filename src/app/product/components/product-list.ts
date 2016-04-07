865fe2ff24d83ce71d6d04251b2cb94d2af452f6
tkarling <tkarling@live.com>
2016-04-06 17:36:05 -0700

    edited item scrolls on top of the page now

--- src/app/product/components/product-list.ts
+++ src/app/product/components/product-list.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from "angular2/core";
 
+import {PageScroll, PageScrollConfig} from 'ng2-page-scroll';
+
 //import {StartedPipe} from "../pipes/started-pipe";
 import {SearchPipe} from "../../search/pipes/search-pipe";
 
@@ -20,18 +22,22 @@ export enum ProductListType {
     directives: [ProductItem, ProductInput],
     template: `
     <style>
+        a:link {
+            text-decoration: none;
+        }
     </style>
     <div class="product-list-container ">
         <!--{{diagnostic}}-->
         <ul class="mdl-list">
-            <li *ngFor="#product of products | search: term">
+            <li *ngFor="#product of products | search: term; #i = index">
+            <a pageScroll href="{{'#moi' + i}}"><span id="{{'moi' + i}}">
             <product-item [hidden]="productService.editing(product)"
                 [product]="product"
                 [checked]="checked(product)"
                 [lineThrough] ="lineThrough"
                 (toggle)="toggle($event)"
                 (remove)="remove(product)"
-                ></product-item>
+                ></product-item></span></a>
             <product-input [hidden]="! productService.editing(product)" [product]="product"
                 (update)="productService.updateProduct(product, $event)"></product-input>
             </li>
@@ -44,6 +50,12 @@ export class ProductList {
     @Input() type: ProductListType;
 
     constructor(public productService:ProductService) {
+        PageScrollConfig.defaultScrollOffset = 0;
+        PageScrollConfig.defaultDuration = 100000;
+        PageScrollConfig.defaultEasingFunction = (t:number, b:number, c:number, d:number):number => {
+            // Linear easing
+            return c * t / d + b;
+        };
     }
 
     get lineThrough() {
@@ -53,6 +65,7 @@ export class ProductList {
     get products() {
         return this.type === ProductListType.shopping ? this.productService.shoppings : this.productService.favorites;
     }
+
 
     checked(product) {
         return this.type === ProductListType.shopping ? product.status === BoughtStatus.bought : product.onList;
